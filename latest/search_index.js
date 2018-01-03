b61ba96cc6133e883f83ad52c7e3b5ec8121bc6a
autodocs <autodocs>
2018-01-03 10:34:52 +0000

    build based on aa71ce7

---
+++ latest/search_index.js
@@ -0,0 +1,59 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": "(Image: Build Status)(Image: Coverage Status)(Image: )(Image: )(Image: Issues)Truss implementation for JuliaFEM."
+},
+
+{
+    "location": "api.html#",
+    "page": "API documentation",
+    "title": "API documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "api.html#API-documentation-1",
+    "page": "API documentation",
+    "title": "API documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "api.html#FEMTruss",
+    "page": "API documentation",
+    "title": "FEMTruss",
+    "category": "Module",
+    "text": "Truss implementation for JuliaFEM. \n\n\n\n"
+},
+
+{
+    "location": "api.html#FEMTruss.Truss",
+    "page": "API documentation",
+    "title": "FEMTruss.Truss",
+    "category": "Type",
+    "text": "This truss fromulation is from\n\n\n\n"
+},
+
+{
+    "location": "api.html#FEMBase.assemble!-Tuple{FEMBase.Assembly,FEMBase.Problem{FEMTruss.Truss},FEMBase.Element{FEMBasis.Seg2},Any}",
+    "page": "API documentation",
+    "title": "FEMBase.assemble!",
+    "category": "Method",
+    "text": "assemble!(assembly:Assembly, problem::Problem{Elasticity}, elements, time)\n\nStart finite element assembly procedure for Elasticity problem. Function groups elements to arrays by their type and assembles one element type at time. This makes it possible to pre-allocate matrices common to same type of elements.\n\n\n\n"
+},
+
+{
+    "location": "api.html#Index-1",
+    "page": "API documentation",
+    "title": "Index",
+    "category": "section",
+    "text": "DocTestSetup = quote\n    using FEMTruss\nendModules = [FEMTruss]"
+},
+
+]}
