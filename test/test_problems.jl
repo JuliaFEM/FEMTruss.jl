45467f073768d574a15972325d5ae45af6cfce7b
jtveiten <jan.tveiten@gmail.com>
2018-02-10 13:30:16 +0100

    Forgot license header again

---
+++ test/test_problems.jl
@@ -0,0 +1,12 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/FEMTruss.jl/blob/master/LICENSE
+
+using FEMBase
+using FEMTruss
+
+using Base.Test
+
+@testset "FEMTrussProblems.jl" begin
+# Do a simple truss system with two trusses in 45 deg.
+# three nodes and and horizontal andf vertical nodes in
+end
